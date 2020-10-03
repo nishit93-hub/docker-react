da7b1e5ae0c1aad76d63db0b38657565ff296085
nishit <nishit.goel@redblink.net>
2020-10-03 13:01:56 +0530

    added travis file

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = render.getByText(/learn react/i);
+  const element = render(<App />);
+  const linkElement = element.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
